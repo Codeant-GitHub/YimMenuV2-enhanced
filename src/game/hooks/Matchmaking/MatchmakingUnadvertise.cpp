8f400ff40da0e7da9e19208f8ddc9682ff6543d5
Codeant <127379751+Codeant-GitHub@users.noreply.github.com>
2025-06-23 16:21:14 +0800

    2025-06-23 16:21:10

---
+++ src/game/hooks/Matchmaking/MatchmakingUnadvertise.cpp
@@ -0,0 +1,21 @@
+//https://github.com/Codeant-GitHub
+
+#pragma once
+#include "common.hpp"
+
+#include "game/hooks/Hooks.hpp"
+#include "core/hooking/DetourHook.hpp"
+#include "game/backend/CustomMatchmaking.hpp"
+
+namespace YimMenu::Hooks
+{
+	bool Matchmaking::MatchmakingUnadvertise(int profile_index, MatchmakingId* id, rage::rlTaskStatus* status)
+	{
+		if (!CustomMatchmaking::OnUnadvertise(id))
+		{
+			status->m_Status = 2;
+			return true;
+		}
+		return BaseHook::Get<Matchmaking::MatchmakingUnadvertise, DetourHook<decltype(&Matchmaking::MatchmakingUnadvertise)>>()->Original()(profile_index, id, status);
+	}
+}
