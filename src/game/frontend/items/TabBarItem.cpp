ea5863873d0ae6fa7eac12c9251ee48bfea784a8
Codeant <127379751+Codeant-GitHub@users.noreply.github.com>
2025-07-08 08:56:46 +0800

    2025-07-08 08:56:10

---
+++ src/game/frontend/items/TabBarItem.cpp
@@ -0,0 +1,27 @@
+//https://github.com/Codeant-GitHub
+
+#pragma once
+#include "common.hpp"
+
+#include "Items.hpp"
+
+namespace YimMenu
+{
+	TabBarItem::TabBarItem(const std::string& id) :
+		m_Id(id)
+	{
+	}
+
+	void TabBarItem::Draw()
+	{
+		if (ImGui::BeginTabBar(m_Id.c_str()))
+		{
+			for (const auto& tab : m_Tabs)
+			{
+				if (tab->CanDraw())
+					tab->Draw();
+			}
+			ImGui::EndTabBar();
+		}
+	}
+}
